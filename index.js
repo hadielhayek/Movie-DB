464bebce6eb123f62756b7c8f64d52eac2f57e8a
hadielhayek <hadi-hayek-20@hotmail.com>
2022-01-09 17:54:36 +0200

    step4

--- index.js
+++ index.js
@@ -14,3 +14,17 @@ app.get('/time', function (req, res) {
     var time = today.getHours() +  ":" + today.getSeconds(); 
     res.status(200).send(time)
 })
+
+app.get('/hello/:id', function (req, res) {
+    let id=req.params.id;
+    res.status(200).send("hello" + id)
+})
+
+app.get('/search?x', function (req, res) {
+    if (req.query.x==" " || req.query.x=="undefined")
+    {
+        res.status(500).send({error:"true",message:"you have to provide a search"})
+    }
+    else{res.status(200).send({message:"ok",data:req.query.x})}
+    
+})
