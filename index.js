dd6853352dda4aa31635a369567e2b30a285afbd
hadielhayek <hadi-hayek-20@hotmail.com>
2022-01-09 17:10:24 +0200

    step3

--- index.js
+++ index.js
@@ -4,4 +4,13 @@ const port = 3000
 
 app.listen(port, () => {
     console.log('ok')
-  })
+})
+
+app.get('/test', function (req, res) {
+    res.status(200).send("ok")
+})
+app.get('/time', function (req, res) {
+    var today = new Date();
+    var time = today.getHours() +  ":" + today.getSeconds(); 
+    res.status(200).send(time)
+})
