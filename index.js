2105601c7af202297b8566c74292efca2ebae46a
hadielhayek <hadi-hayek-20@hotmail.com>
2022-01-09 18:19:17 +0200

    step5

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 const express = require('express')
 const app = express()
 const port = 3000
-
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
 app.listen(port, () => {
     console.log('ok')
 })
@@ -27,4 +32,20 @@ app.get('/search?x', function (req, res) {
     }
     else{res.status(200).send({message:"ok",data:req.query.x})}
     
+})
+
+app.get('/read', function (req, res) {
+    res.status(200).send(movies)
+})
+
+app.post('/create', function (req, res) {
+    res.status(200).send("create")
+})
+
+app.put('/update', function (req, res) {
+    res.status(200).send("update")
+})
+
+app.delete('/delete', function (req, res) {
+    res.status(200).send("delete")
 })
